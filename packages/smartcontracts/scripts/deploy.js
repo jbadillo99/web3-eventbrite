e879eebe0260f55d4b51f1fc6c54a271c1d1f766
Jesus Badillo <jbadillo2199@gmail.com>
2022-08-07 13:54:07 -0400

    fixed contract structure to make monorepo

---
+++ packages/smartcontracts/scripts/deploy.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+const main = async() => {
+  const rsvpContractFactory = await hre.ethers.getContractFactory("Web3EventBrite");
+  const rsvpContract = await rsvpContractFactory.deploy();
+  await rsvpContract.deployed();
+  console.log("Contract deployed to: ", rsvpContract.address);
+
+};
+
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
