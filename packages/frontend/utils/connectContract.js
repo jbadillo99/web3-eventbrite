92609bdd4a558e7840d91f382b8fc100da98c61b
Jesus Badillo <jbadillo2199@gmail.com>
2022-08-23 20:56:27 -0500

    add contract connection to using ether.js

--- packages/frontend/utils/connectContract.js
+++ packages/frontend/utils/connectContract.js
@@ -4,15 +4,17 @@ import { ethers } from "ethers";
 
 // Try to access global ethereum API and return the rsvpContract with the correct contract configurations
 function connectContract() {
-    const contractAddress = "0x7785e0a34C04c1c87269FF8469a68e0EE2867a93";
+    const contractAddress = "0x7C21e525258a5D4F8e9abE60F4CbA07B2168ee9d";
     const contractABI = abiJSON.abi;
 
     let rsvpContract;
 
     try {
-        const {ethereum} = window;
 
-        if(ethereum) {
+        // Access ethereum API using the window object
+        const { ethereum } = window;
+
+        if (ethereum) {
 
             // Check for Eth object in window
             const provider = new ethers.providers.Web3Provider(ethereum);
@@ -20,7 +22,7 @@ function connectContract() {
 
             rsvpContract = new ethers.Contract(contractAddress, contractABI, signer);
         } else {
-            console.log("Ethereum object does'nt exist");
+            console.log("Ethereum object doesn't exist");
         }
 
     } catch(err) {
@@ -28,4 +30,4 @@ function connectContract() {
     }
     return rsvpContract;
 }
-export default connectContract();
+export default connectContract;
